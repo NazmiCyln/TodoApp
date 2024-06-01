e3baaa5cc6b7578547eb775f42cb24bd7a8d15e3
Nazmi Ceylan <nazmican50@gmail.com>
2024-06-01 14:36:57 +0300

    Firebase Service added

--- lib/env.dart
+++ lib/env.dart
@@ -3,7 +3,7 @@ import 'package:envied/envied.dart';
 part 'env.g.dart';
 
 @Envied(path: '.env')
-abstract class Env {
-  @EnviedField(varName: 'SECRET_KEY')
-  static const String secretKey = _Env.secretKey;
+abstract  class Env {
+  @EnviedField(obfuscate: true, varName: 'SECRET_KEY')
+  static  String secretKey = _Env.secretKey;
 }
